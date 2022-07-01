953a4e79e1a86ac7b346ba81baa32bcab6ca2596
William White <billwhite@RCPCONSULTING.BIZ>
2022-07-01 18:29:54 -0400

    Through Chapter 3

---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your fucking name.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your fucking email address.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your fucking phone number.")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Well, are you going to fucking join us or not?")]
+        public bool? WillAttend { get; set; }
+    }
+}
